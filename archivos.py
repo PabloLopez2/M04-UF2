90c5f42acf77a7e6913b8a6efa550341a8eb0048
pablo.lopez.socias@enti.cat <pablo.lopez.socias@enti.cat>
2023-04-18 10:29:37 +0200

    actualizacion

--- archivos.py
+++ archivos.py
@@ -2,14 +2,19 @@
 #Objeto que contiene la función read. (Se añade 1 salto de línea)
 #read(8) escribe los 8 primeros carácteres.
 
-archivo = open("numeritos.txt", "r")
 
 #print(archivo.readline(8))
-
 #Podemos ponerle delimitador
-linea = archivo.readline()
-lista = linea.split(";")
-print(lista[2])
+
+try:
+	archivo = open("numeritos.txt", "r")
+#print(archivo.readline())
+	linea = archivo.readline()
+	lista = linea.split(";")
+	print(lista[3])
+	archivo.close()
+except:
+	print("Error al abrir el archivo")
 
 archivo.close()
 
